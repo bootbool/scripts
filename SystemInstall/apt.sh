564c1c1e37774bd4ed350093327f9f5dd782ac86
evan jiang <915086731@qq.com>
2024-11-21 10:44:02 +0800

    Create apt.sh
    kernel/ebpf

---
+++ SystemInstall/apt.sh
@@ -0,0 +1,10 @@
+#kernel
+sudo apt install libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf llvm
+sudo apt install git
+
+#ebpf
+sudo apt install -y make clang llvm libelf-dev libbpf-dev bpfcc-tools libbpfcc-dev
+sudo apt install -y libelf-dev libpcap-dev libbfd-dev binutils-dev build-essential make
+
+
+
